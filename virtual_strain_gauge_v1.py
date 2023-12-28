0f7339bdf9a2127946cbc0ff61e63bc17afaf5a0
emreatay993 <63810700+emreatay993@users.noreply.github.com>
2023-12-29 02:45:11 +0300

    Add files via upload

---
+++ virtual_strain_gauge_v1.py
@@ -0,0 +1,200 @@
+import context_menu
+
+# TODO - SELECT STRAIN GAGE BY USER
+file_path_of_selected_SG_Part_from_library = r"C:\Users\emre_\VS_Code\strain_gauge_calculations\SG_Micro_AE1.pmdb"
+
+# TODO - Select Strain Gage By User
+obj_of_solution = DataModel.GetObjectsByName("Solution")[0]
+
+#Instances
+create_geo_selection = ExtAPI.SelectionManager.CreateSelectionInfo(SelectionTypeEnum.GeometryEntities)
+
+
+# Extracting reference points and their IDs for strain gauges.
+list_of_obj_of_CS_SG_ref_points = [
+    Model.CoordinateSystems.Children[i]
+    for i in range(len(Model.CoordinateSystems.Children))
+    if Model.CoordinateSystems.Children[i].Name.Contains("CS_SG_Ref")
+]
+
+list_of_IDs_of_CS_SG_ref_points = [
+    Model.CoordinateSystems.Children[i].ObjectId
+    for i in range(len(Model.CoordinateSystems.Children))
+    if Model.CoordinateSystems.Children[i].Name.Contains("CS_SG_Ref")
+]
+
+# Importing and configuring geometry for each strain gauge.
+for i in range(len(list_of_IDs_of_CS_SG_ref_points)):
+    geometry_import_group = Model.GeometryImportGroup
+    geometry_import = geometry_import_group.AddGeometryImport()
+    geometry_import.Name = "SG_" + str(i + 1) +"_Geometry_Import"
+    geometry_import_format = Ansys.Mechanical.DataModel.Enums.GeometryImportPreference.Format.Automatic
+    geometry_import_preferences = Ansys.ACT.Mechanical.Utilities.GeometryImportPreferences()
+    
+    geometry_import.Import(file_path_of_selected_SG_Part_from_library, geometry_import_format, geometry_import_preferences)
+    
+    list_of_obj_of_bodies = DataModel.GetObjectsByType(Ansys.ACT.Automation.Mechanical.Body)
+    list_of_IDs_of_underdefined_SG_grids = [
+        list_of_obj_of_bodies[k].ObjectId
+        for k in range(len(list_of_obj_of_bodies))
+        if (list_of_obj_of_bodies[k].Thickness == Quantity('0 [m]') and 
+            list_of_obj_of_bodies[k].Material == '' and 
+            list_of_obj_of_bodies[k].Name.Contains("SG_"))
+    ]
+    
+    # Updating properties of underdefined gauges.
+    j = 1
+    for each_ID_of_underdefined_SG_grids in list_of_IDs_of_underdefined_SG_grids:
+        tree_object_of_gauge = DataModel.GetObjectById(each_ID_of_underdefined_SG_grids)
+        tree_object_of_gauge.StiffnessOption = ShellElementStiffnessOption.StressEvaluationOnly
+        tree_object_of_gauge.Material = "Structural Steel"
+        tree_object_of_gauge.Color = 55295
+        tree_object_of_gauge.Name = "SG_Grid_Body_" + str(i + 1) + "_" + str(j)
+        
+        # Add local grid coordinates and orient them
+        CS_SG_grid = Model.CoordinateSystems.AddCoordinateSystem()
+        CS_SG_grid.Name = "CS_Ch_" + str(i + 1) + "_" + str(j)
+        body_selection_single_channel_of_an_SG = create_geo_selection
+        body_selection_single_channel_of_an_SG.Ids = [tree_object_of_gauge.GetGeoBody().Id]
+        face_selection_single_channel_of_an_SG = create_geo_selection
+        face_selection_single_channel_of_an_SG.Ids = [tree_object_of_gauge.GetGeoBody().Faces[0].Id]
+        CS_SG_grid.OriginLocation = body_selection_single_channel_of_an_SG
+        CS_SG_grid.PrimaryAxis = CoordinateSystemAxisType.PositiveZAxis
+        CS_SG_grid.PrimaryAxisDefineBy = CoordinateSystemAlignmentType.Associative
+        CS_SG_grid.PrimaryAxisLocation = face_selection_single_channel_of_an_SG
+        
+        # Find the long edge of the grid 
+        grid_edge_lengths=[tree_object_of_gauge.GetGeoBody().Edges[m].Length 
+                           for m in range(len(tree_object_of_gauge.GetGeoBody().Edges))]
+        # Index and ID of the longest edge of the grid
+        m_of_longest_edge = grid_edge_lengths.index(max(grid_edge_lengths))
+        ID_of_longest_edge = tree_object_of_gauge.GetGeoBody().Edges[m_of_longest_edge].Id
+        
+        # Use the long edge of the grid to orient the local grid CS
+        long_edge_selection_single_channel_of_an_SG = create_geo_selection
+        long_edge_selection_single_channel_of_an_SG.Ids = [ID_of_longest_edge]
+        CS_SG_grid.SecondaryAxis = CoordinateSystemAxisType.PositiveXAxis
+        CS_SG_grid.SecondaryAxisDefineBy = CoordinateSystemAlignmentType.Associative
+        CS_SG_grid.SecondaryAxisLocation = long_edge_selection_single_channel_of_an_SG
+        
+        # Ensure that the directions of secondary axes are correct and are initially towards the preferred axis
+        string_of_CS_SG_grid_direction_vector_X = CS_SG_grid.XAxisData
+        string_of_CS_SG_grid_direction_vector_X = string_of_CS_SG_grid_direction_vector_X.strip('[]').split()
+        list_of_CS_SG_grid_direction_vector_X = [float(num) for num in string_of_CS_SG_grid_direction_vector_X]
+        # Flip the local grid X if it is in the negative global X direction 
+        if list_of_CS_SG_grid_direction_vector_X[0] < 0:
+            CS_SG_grid.FlipX()
+            CS_SG_grid.FlipZ() # Correct Z direction due to X flip
+        
+        # Add element orientations to each grid
+        ExtAPI.SelectionManager.ClearSelection()
+        tree_object_of_gauge = DataModel.GetObjectById(each_ID_of_underdefined_SG_grids)
+        body_selection_single_channel_of_an_SG.Ids = [tree_object_of_gauge.GetGeoBody().Id]
+        
+        grid_element_orientation = Model.Geometry.AddElementOrientation()
+        grid_element_orientation.Name = "Orient_CS_SG_Ch" + str(i + 1) + "_" + str(j)
+        grid_element_orientation.OrientationGuideDefinedBy = ElementOrientationGuide.CoordinateSystemGuide
+        grid_element_orientation.CoordinateSystem = CS_SG_grid
+        grid_element_orientation.BodyLocation = body_selection_single_channel_of_an_SG
+        
+        normal_strain_x_SG_grid = obj_of_solution.AddNormalElasticStrain()
+        normal_strain_x_SG_grid.Location = body_selection_single_channel_of_an_SG
+        normal_strain_x_SG_grid.CoordinateSystem = None  # Results will be in Solution CS
+        normal_strain_x_SG_grid.Name = "StrainX_SG_" + str(i + 1) + "_" + str(j)
+        
+        j += 1
+    
+    # Get the body selection of grids
+    body_selection_all_channels_of_an_SG = ExtAPI.SelectionManager.CreateSelectionInfo(SelectionTypeEnum.GeometryEntities)
+    body_selection_all_channels_of_an_SG.Ids = [
+        DataModel.GetObjectById(each_ID_of_underdefined_SG_grids).GetGeoBody().Id
+        for each_ID_of_underdefined_SG_grids in list_of_IDs_of_underdefined_SG_grids
+    ]
+    
+    # Configuring part transformation.
+    part_transform = Model.AddPartTransform()
+    part_transform.Name = "SG_" + str(i + 1) + "_Geometry_Placement"
+    part_transform.Location = body_selection_all_channels_of_an_SG
+    part_transform.DefineBy = PartTransformationDefinitionType.CoordinateSystem
+    part_transform.TargetCoordinateSystem = list_of_obj_of_CS_SG_ref_points[i]
+
+# Regenerating contacts and transforming mesh.
+part_transform_group = Model.PartTransformGroup
+part_transform_group.RegenerateContacts = True
+part_transform_group.TransformMesh = True
+
+context_menu.DoTransformGeometry(ExtAPI)
+
+
+
+def group_SG_objects_created():
+    
+    # Create the lists of objects of different types to group
+    list_of_obj_of_CS_Ch_of_grids = [
+        Model.CoordinateSystems.Children[i]
+        for i in range(len(Model.CoordinateSystems.Children))
+        if Model.CoordinateSystems.Children[i].Name.Contains("CS_Ch")]
+    
+    list_of_obj_all_element_orientations = DataModel.Project.GetChildren(DataModelObjectCategory.ElementOrientation,True)
+    list_of_obj_of_grid_element_orientations = [
+        list_of_obj_all_element_orientations[i]
+        for i in range(len(list_of_obj_all_element_orientations))
+        if list_of_obj_all_element_orientations[i].Name.Contains("Orient_CS_SG_Ch")]
+    
+    list_of_obj_of_all_bodies = DataModel.Project.GetChildren(DataModelObjectCategory.Body,True)
+    list_of_obj_of_SG_grid_bodies = [
+        list_of_obj_of_all_bodies[i]
+        for i in range(len(list_of_obj_of_all_bodies))
+        if list_of_obj_of_all_bodies[i].Name.Contains("SG_Grid_Body_")
+        and list_of_obj_of_all_bodies[i].Thickness == Quantity('0 [m]')
+        and list_of_obj_of_all_bodies[i].ObjectState == ObjectState.FullyDefined]
+    
+    list_of_obj_of_all_part_transforms = DataModel.Project.GetChildren(DataModelObjectCategory.PartTransform,True)
+    list_of_obj_of_SG_part_transforms_initial_placements = [
+        list_of_obj_of_all_part_transforms[i]
+        for i in range(len(list_of_obj_of_all_part_transforms))
+        if list_of_obj_of_all_part_transforms[i].Name.Contains("SG_")
+        and list_of_obj_of_all_part_transforms[i].Name.Contains("Geometry_Placement")]
+        
+    list_of_obj_of_all_geometry_imports = DataModel.Project.GetChildren(DataModelObjectCategory.GeometryImport,True)
+    list_of_obj_of_SG_geometry_imports = [
+        list_of_obj_of_all_geometry_imports[i]
+        for i in range(len(list_of_obj_of_all_geometry_imports))
+        if list_of_obj_of_all_geometry_imports[i].Name.Contains("SG_")
+        and list_of_obj_of_all_geometry_imports[i].Name.Contains("_Geometry_Import")]
+    
+    list_of_obj_of_all_elastic_strains = DataModel.Project.GetChildren(DataModelObjectCategory.NormalElasticStrain,True)
+    list_of_obj_of_SG_grid_strains = [
+        list_of_obj_of_all_elastic_strains[i]
+        for i in range(len(list_of_obj_of_all_elastic_strains))
+        if list_of_obj_of_all_elastic_strains[i].Name.Contains("StrainX_SG_")]
+    
+    # Group the coordinate systems of each grid channel
+    ExtAPI.DataModel.Tree.Activate(list_of_obj_of_CS_Ch_of_grids)
+    context_menu.DoCreateGroupingFolderInTree(ExtAPI)
+    DataModel.GetObjectsByName("New Folder")[0].Name = "CS_Ch"
+    
+    # Group the element orientations created
+    ExtAPI.DataModel.Tree.Activate(list_of_obj_of_grid_element_orientations)
+    context_menu.DoCreateGroupingFolderInTree(ExtAPI)
+    DataModel.GetObjectsByName("New Folder")[0].Name = "Orient_CS_SG_Ch"
+    
+    # Group the SG grid bodies created
+    ExtAPI.DataModel.Tree.Activate(list_of_obj_of_SG_grid_bodies)
+    context_menu.DoCreateGroupingFolderInTree(ExtAPI)
+    DataModel.GetObjectsByName("New Folder")[0].Name = "SG_Grid_Body"
+    
+    # Group the SG geometry imports from library
+    ExtAPI.DataModel.Tree.Activate(list_of_obj_of_SG_geometry_imports)
+    context_menu.DoCreateGroupingFolderInTree(ExtAPI)
+    DataModel.GetObjectsByName("New Folder")[0].Name = "SG_Geometry_Imports"
+    
+    # Group the strain result objects of SG grids
+    ExtAPI.DataModel.Tree.Activate(list_of_obj_of_SG_grid_strains)
+    context_menu.DoCreateGroupingFolderInTree(ExtAPI)
+    DataModel.GetObjectsByName("New Folder")[0].Name = "StrainX_SG"
+    
+group_SG_objects_created()
+
+
+# TODO Her bir local grid CS ye ayni oryantasyona sahip strain probelar ekle
