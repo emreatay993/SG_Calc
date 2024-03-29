21ccd01cce34779fc72a6cafd07b3808d159ff8a
emreatay993 <63810700+emreatay993@users.noreply.github.com>
2024-03-29 20:58:26 +0300

    Add files via upload
    Creates or defines the named selection of the faces of test parts to be used as sensor mounting surfaces in subsequent operations of this toolbox. If the named selection was not created before using this button, ensure that this button is re-run after the required named selection is generated and populated.

--- define_NS_of_SG_test_parts.py
+++ define_NS_of_SG_test_parts.py
@@ -7,31 +7,31 @@ list_of_names_of_NS_children = [Model.NamedSelections.Children[i].Name
                                 for i in range(len(Model.NamedSelections.Children))]
 
 try:
-    NS_of_bodies_of_SG_test_parts = DataModel.GetObjectsByName("NS_of_bodies_of_SG_test_parts")[0]
-    NS_of_bodies_of_SG_test_parts_not_found = False
+    NS_of_faces_of_SG_test_parts = DataModel.GetObjectsByName("NS_of_faces_of_SG_test_parts")[0]
+    NS_of_faces_of_SG_test_parts_not_found = False
 except:
-    NS_of_bodies_of_SG_test_parts_not_found = True
+    NS_of_faces_of_SG_test_parts_not_found = True
 
 
-if (NS_of_bodies_of_SG_test_parts_not_found):
+if (NS_of_faces_of_SG_test_parts_not_found):
     
     # Define the message, caption, and buttons for the message box
-    message = r"""Please define a named selection of bodies called "NS_of_bodies_of_SG_test_parts" and re-run this button to create contacts between strain gauges and test parts automatically. """
+    message = r"""Please define a named selection of bodies called "NS_of_faces_of_SG_test_parts" and re-run this button to create contacts between strain gauges and test parts automatically. """
     caption = "Warning"
     buttons = MessageBoxButtons.OK
     icon = MessageBoxIcon.Warning
     # Show the message box
     result = MessageBox.Show(message, caption, buttons, icon)
 
-    NS_of_bodies_of_SG_test_parts = Model.AddNamedSelection()
-    NS_of_bodies_of_SG_test_parts.Name = "NS_of_bodies_of_SG_test_parts"
-    NS_of_bodies_of_SG_test_parts.SendToSolver = False
+    NS_of_faces_of_SG_test_parts = Model.AddNamedSelection()
+    NS_of_faces_of_SG_test_parts.Name = "NS_of_faces_of_SG_test_parts"
+    NS_of_faces_of_SG_test_parts.SendToSolver = False
     
-if (NS_of_bodies_of_SG_test_parts_not_found == False and
-    len(NS_of_bodies_of_SG_test_parts.Location.Ids) == 0):
+if (NS_of_faces_of_SG_test_parts_not_found == False and
+    len(NS_of_faces_of_SG_test_parts.Location.Ids) == 0):
       
     # Define the message, caption, and buttons for the message box
-    message = r""""NS_of_bodies_of_SG_test_parts" object is already in the tree but the bodies of test parts are not defined. 
+    message = r""""NS_of_faces_of_SG_test_parts" object is already in the tree but the bodies of test parts are not defined. 
     
 Ensure that it is assigned and this button is re-run to correct parts to create contacts between strain gauges and test parts automatically. """
 
@@ -42,11 +42,11 @@ Ensure that it is assigned and this button is re-run to correct parts to create 
     result = MessageBox.Show(message, caption, buttons, icon)
 
 
-if (NS_of_bodies_of_SG_test_parts_not_found == False and
-    len(NS_of_bodies_of_SG_test_parts.Location.Ids) != 0):
+if (NS_of_faces_of_SG_test_parts_not_found == False and
+    len(NS_of_faces_of_SG_test_parts.Location.Ids) != 0):
       
     # Define the message, caption, and buttons for the message box
-    message = r""""NS_of_bodies_of_SG_test_parts" object is already defined. Ensure that it is assigned to correct parts to create contacts between strain gauges and test parts automatically. """
+    message = r""""NS_of_faces_of_SG_test_parts" object is already defined. Ensure that it is assigned to correct parts to create contacts between strain gauges and test parts automatically. """
 
     caption = "Warning"
     buttons = MessageBoxButtons.OK
@@ -54,5 +54,5 @@ if (NS_of_bodies_of_SG_test_parts_not_found == False and
     # Show the message box
     result = MessageBox.Show(message, caption, buttons, icon)
     
-    list_of_IDs_of_selections_of_NS_of_bodies_of_SG_test_parts = NS_of_bodies_of_SG_test_parts.Location.Ids
+    list_of_IDs_of_selections_of_NS_of_faces_of_SG_test_parts = NS_of_faces_of_SG_test_parts.Location.Ids
       
