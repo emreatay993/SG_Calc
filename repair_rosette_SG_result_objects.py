8c5c84797ea4276a97ac32bd64016b14523a1e23
emreatay993 <63810700+emreatay993@users.noreply.github.com>
2024-04-07 02:54:23 +0300

    Add files via upload

--- repair_rosette_SG_result_objects.py
+++ repair_rosette_SG_result_objects.py
@@ -1,104 +1,85 @@
 # region Import necessary libraries
 import re
+import context_menu
 # endregion
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# region Functions for object name/number checking
-def extract_grid_and_channel_numbers(names_list, pattern):
-    list_of_SG_ref_numbers = []
-    list_of_SG_ch_numbers = []
-    
-    for name in names_list:
-        match = re.search(pattern, name)
-        if match:
-            list_of_SG_ref_numbers.append(int(match.group(1)))
-            list_of_SG_ch_numbers.append(int(match.group(2)))
-    
-    return list_of_SG_ref_numbers, list_of_SG_ch_numbers
-
-#Function to compare lists of reference and channel numbers and find missing elements
-def find_missing_elements(ref_numbers_1, ch_numbers_1, ref_numbers_2, ch_numbers_2):
-    # Pairing reference and channel numbers as tuples
-    paired_list_1 = set(zip(ref_numbers_1, ch_numbers_1))
-    paired_list_2 = set(zip(ref_numbers_2, ch_numbers_2))
-    
-    # Finding missing elements
-    missing_in_list_2 = paired_list_1 - paired_list_2  # Elements in list 1 not in list 2
-    missing_in_list_1 = paired_list_2 - paired_list_1  # Elements in list 2 not in list 1
-    
-    return list(missing_in_list_1), list(missing_in_list_2)
-# endregion
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-
-# region Extract the channel names of SG_Grid_Body reference points that exist in the tree
+# region Get the list of unsuppressed SG_Grid_Body  and their names in the tree
 list_of_obj_of_all_bodies = DataModel.Project.GetChildren(DataModelObjectCategory.Body,True)
-list_of_names_of_SG_Grid_Body_of_grids = [
+list_of_obj_of_SG_grid_bodies = [
+    list_of_obj_of_all_bodies[i]
+    for i in range(len(list_of_obj_of_all_bodies))
+    if list_of_obj_of_all_bodies[i].Name.Contains("SG_Grid_Body_")
+    and list_of_obj_of_all_bodies[i].Thickness == Quantity('0 [m]')
+    and list_of_obj_of_all_bodies[i].ObjectState == ObjectState.FullyDefined]
+    
+list_of_names_of_SG_grid_bodies = [
     list_of_obj_of_all_bodies[i].Name
     for i in range(len(list_of_obj_of_all_bodies))
-    if list_of_obj_of_all_bodies[i].Name.Contains("SG_Grid_Body")
-    and list_of_obj_of_all_bodies[i].DataModelObjectCategory != DataModelObjectCategory.TreeGroupingFolder]
+    if list_of_obj_of_all_bodies[i].Name.Contains("SG_Grid_Body_")
+    and list_of_obj_of_all_bodies[i].Thickness == Quantity('0 [m]')
+    and list_of_obj_of_all_bodies[i].ObjectState == ObjectState.FullyDefined]
 # endregion
 
-# region Extract the channel names of StrainX results that exist in the tree
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# region Get the list of unsuppressed SG_Grid_Body objects in the tree and remove them
 list_of_obj_of_all_elastic_strains = DataModel.Project.GetChildren(DataModelObjectCategory.NormalElasticStrain,True)
-list_of_names_of_SG_grid_strains = [
-    list_of_obj_of_all_elastic_strains[i].Name
+list_of_obj_of_SG_grid_strains = [
+    list_of_obj_of_all_elastic_strains[i]
     for i in range(len(list_of_obj_of_all_elastic_strains))
     if list_of_obj_of_all_elastic_strains[i].Name.Contains("StrainX_SG")]
+
+DataModel.Tree.Activate(list_of_obj_of_SG_grid_strains)
+if len(list_of_obj_of_SG_grid_strains) > 0:
+    context_menu.DoEditDelete(ExtAPI)
 # endregion
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# region Extract the reference and channel number SG_Grid_Body reference points that exist in the tree
-pattern_StrainX_SG = r"StrainX_SG(\d+)_(\d+)"
-list_of_StrainX_SG_ref_numbers, list_of_StrainX_SG_ch_numbers = extract_grid_and_channel_numbers(list_of_names_of_SG_grid_strains, pattern_StrainX_SG)
-# endregion
-
-# region Extract the reference and channel number SG_Grid_Body reference points that exist in the tree
-pattern_SG_Grid_Body = r"SG_Grid_Body_(\d+)_(\d+)"
-list_of_SG_Grid_Body_ref_numbers, list_of_SG_Grid_Body_numbers = extract_grid_and_channel_numbers(list_of_names_of_SG_Grid_Body_of_grids, pattern_SG_Grid_Body)
-# endregion
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-
-# region Finding only the missing elements in list_of_StrainX_SG compared to list_of_CS_SG
-missing_in_list_of_StrainX = \
-find_missing_elements(list_of_StrainX_SG_ref_numbers, 
-                      list_of_StrainX_SG_ch_numbers, 
-                      list_of_SG_Grid_Body_ref_numbers, 
-                      list_of_SG_Grid_Body_numbers)[0]
-# endregion
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-
-# region Filter elements of list_of_names_of_SG_Grid_Bodies based on missing elements in missing_in_list_of_StrainX
-list_of_filtered_names_of_SG_Grid_Bodies = [
-    name for name in list_of_names_of_SG_Grid_Body_of_grids
-    if any("SG_Grid_Body_{}_{}".format(ref, ch) in name for ref, ch in missing_in_list_of_StrainX)]
-# endregion
-
-# region Filter elements of list_of_names_of_SG_Grid_Bodies based on missing elements in missing_in_list_of_StrainX
-list_of_filtered_names_of_missing_StrainX_SG = [
-    "StrainX_SG{}_{}".format(ref, ch) for ref, ch in missing_in_list_of_StrainX]
+# region Get the list of StrainX_SG objects to be added
+list_of_names_of_SG_grid_strains = [name.replace("SG_Grid_Body", "StrainX_SG") for name in list_of_names_of_SG_grid_bodies]
 # endregion
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # region Add StrainX_SG results that are missing in the tree
-for i in range(len(list_of_filtered_names_of_missing_StrainX_SG)):
+for i in range(len(list_of_names_of_SG_grid_strains)):
     # region Add strain results for each SG grid for postprocessing time vs strain data in each channel
     body_selection_of_missing_SG_strainx_results = \
     ExtAPI.SelectionManager.CreateSelectionInfo(SelectionTypeEnum.GeometryEntities)
     body_selection_of_missing_SG_strainx_results.Ids = [
-        DataModel.GetObjectsByName(list_of_filtered_names_of_SG_Grid_Bodies[i])[0].GetGeoBody().Id]
+        DataModel.GetObjectsByName(list_of_names_of_SG_grid_bodies[i])[0].GetGeoBody().Id]
     
     normal_strain_x_SG_grid = sol_selected_environment.AddNormalElasticStrain()
     normal_strain_x_SG_grid.Location = body_selection_of_missing_SG_strainx_results
     normal_strain_x_SG_grid.CoordinateSystem = None  # Results will be in Solution CS
     normal_strain_x_SG_grid.CalculateTimeHistory = True
-    normal_strain_x_SG_grid.Name = list_of_filtered_names_of_missing_StrainX_SG[i]
+    normal_strain_x_SG_grid.Name = list_of_names_of_SG_grid_strains[i]
 # endregion
 
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
 
+# region Find the existing StrainX_SG group, 
+list_of_obj_of_tree_group_of_StrainX_SG = [
+    DataModel.GetObjectsByName(sol_selected_environment.Children[i].Name)[0]
+    for i in range(len(sol_selected_environment.Children))
+    if sol_selected_environment.Children[i].Name.Contains("StrainX_SG")
+    and sol_selected_environment.Children[i].DataModelObjectCategory == DataModelObjectCategory.TreeGroupingFolder]
+
+# Ungroup the folder and its contents
+[list_of_obj_of_tree_group_of_StrainX_SG[i].Ungroup() 
+for i in range(len(list_of_obj_of_tree_group_of_StrainX_SG))]
+
+# Get the updated list of all StrainX_SG objects in the tree
+list_of_obj_of_all_elastic_strains = DataModel.Project.GetChildren(DataModelObjectCategory.NormalElasticStrain,True)
+list_of_obj_of_SG_grid_strains = [
+    list_of_obj_of_all_elastic_strains[i]
+    for i in range(len(list_of_obj_of_all_elastic_strains))
+    if list_of_obj_of_all_elastic_strains[i].Name.Contains("StrainX_SG")]
+
+# Group existing StrainX_SG objects back again
+ExtAPI.DataModel.Tree.Activate(list_of_obj_of_SG_grid_strains)
+context_menu.DoCreateGroupingFolderInTree(ExtAPI)
+DataModel.GetObjectsByName("New Folder")[0].Name = "StrainX_SG"
