0418f75dbc20af1254907f9cd81398f0cb35cb24
emreatay993 <63810700+emreatay993@users.noreply.github.com>
2024-04-07 15:16:07 +0300

    Add files via upload

--- extract_SG_grid_strain_tabular_data.py
+++ extract_SG_grid_strain_tabular_data.py
@@ -86,8 +86,8 @@ for m in range(len(list_of_names_of_SG_grid_strains)):
     columns = [numeric_list[i * num_elements_per_column: (i + 1) * num_elements_per_column] for i in range(5)]
 
     # Extract and modify the names of each column data
-    headers_strain.append(list_of_obj_of_SG_grid_strains[m].Name.Substring(8))
-    headers_microstrain.append(list_of_obj_of_SG_grid_strains[m].Name.Substring(8))
+    headers_strain.append(list_of_names_of_SG_grid_strains[m].Substring(8))
+    headers_microstrain.append(list_of_names_of_SG_grid_strains[m].Substring(8))
 
     # Assuming each inner list of columns is a separate column of data
     list_of_strain_data.append(columns[4])
